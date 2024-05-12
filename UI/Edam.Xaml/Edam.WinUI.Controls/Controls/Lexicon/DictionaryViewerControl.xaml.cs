d7e8b5ab580832de08b3b966a5233050b167d944
Eduardo Sobrino <esobrino@openk.com>
2024-05-12 17:26:33 -0400

    Added Dictionary Viewer Control...

---
+++ UI/Edam.Xaml/Edam.WinUI.Controls/Controls/Lexicon/DictionaryViewerControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+using Edam.WinUI.Controls.DataModels;
+
+namespace Edam.WinUI.Controls.Lexicon
+{
+
+   public sealed partial class DictionaryViewerControl : UserControl
+   {
+      private DictionariesModel m_ViewModel = new DictionariesModel();
+      public DictionariesModel ViewModel
+      {
+         get { return m_ViewModel; }
+         set { m_ViewModel = value; }
+      }
+
+      public DictionaryViewerControl()
+      {
+         this.InitializeComponent();
+         DataContext = m_ViewModel;
+      }
+
+   }
+
+}
